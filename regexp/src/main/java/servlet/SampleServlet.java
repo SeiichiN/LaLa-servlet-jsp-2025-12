303a6bd7ba8a951b65776f90779da84014083aaa
SeiichiN <billie175@gmail.com>
2025-12-18 17:13:29 +0900

    正規表現

--- regexp/src/main/java/servlet/SampleServlet.java
+++ regexp/src/main/java/servlet/SampleServlet.java
@@ -1,8 +1,8 @@
-package main;
+package servlet;
 
 import java.io.IOException;
+import java.util.regex.Pattern;
 
-import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -12,17 +12,20 @@ import jakarta.servlet.http.HttpServletResponse;
 @WebServlet("/SampleServlet")
 public class SampleServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String url = "sample.jsp";
-		RequestDispatcher d =
-				request.getRequestDispatcher(url);
-		d.forward(request, response);
-		
-	}
+	
+	private static final Pattern ID_PATTERN = 
+			Pattern.compile("^[0-9a-zA-Z]{4}$");
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		request.setCharacterEncoding("UTF-8");
+		String id = request.getParameter("id");
+		
+		String msg = "";
+		if (ID_PATTERN.matcher(id).matches()) {
+			msg = "ようこそ、" + id + "さん";
+		} else {
+			msg = "idが違います";
+		}
+		System.out.println(msg);
 	}
-
 }
